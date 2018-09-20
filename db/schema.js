24f031410a8822cdd9de95931dbdfa210c45ddf2
elfein <elfein99@gmail.com>
2018-09-20 09:15:21 -0400

    set up initial Schemas for Item, Collection, and User

---
+++ db/schema.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const ItemSchema = new Schema({
+    name: String,
+    origin: String,
+    getYear: Number,
+    note: String,
+    isGift: Boolean
+})
+
+const CollectionSchema = new Schema({
+    name: String,
+    startYear: Number,
+    note: String,
+    items: [ItemSchema]
+})
+
+const UserSchema = new Schema({
+    name: String,
+    collections: [CollectionSchema]
+})
+
+const ItemModel = mongoose.model('Item', ItemSchema)
+const CollectionModel = mongoose.model('Collection', CollectionSchema)
+const UserModel = mongoose.model('User', UserSchema)
+
+module.exports = { Item: ItemModel, Collection: CollectionModel, User: UserModel }
