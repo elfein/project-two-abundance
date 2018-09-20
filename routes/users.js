20b3aa1b7597b6dd4bc93be34ed84e1664405f6e
elfein <elfein99@gmail.com>
2018-09-20 11:12:50 -0400

    added get routes for edit and new user pages, and rendered hbs for each

--- routes/users.js
+++ routes/users.js
@@ -3,28 +3,43 @@ var router = express.Router();
 const { User } = require('../db/schema')
 
 // INDEX - SHOW ALL
-router.get('/', function(req, res) {
+router.get('/', function (req, res) {
   User.find()
-  .then((users) => {
-    res.render('users/index', { users })
-  })
+    .then((users) => {
+      res.render('users/index', { users })
+    })
 });
 
 // NEW
+router.get('/new', (req, res) => {
+  res.render('users/new')
+})
 
 // SHOW ONE
 router.get('/:id', (req, res) => {
   User.findById(req.params.id)
-  .then((user) => {
-    res.render('users/show', { user })
-  })
+    .then((user) => {
+      res.render('users/show', { user })
+    })
 })
 
 // CREATE
 
 // EDIT
+router.get('/:id/edit', (req, res) => {
+  User.findById(req.params.id)
+    .then((user) => {
+      res.render('users/edit', { user })
+    })
+})
 
 // UPDATE
+// router.put('/:id', (req, res) => {
+//   User.findByIdAndUpdate(req.params.id, req.boy)
+//   .then((user) => {
+//     res.redirect(`/users/${user._id}`)
+//   })
+// })
 
 // DELETE
 
