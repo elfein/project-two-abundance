5d56701749926ddf3e1e4a225bd5905b288ab08d
elfein <elfein99@gmail.com>
2018-09-20 09:58:42 -0400

    set up show all users GET route

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+const { User } = require('../db/schema')
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+// SHOW ALL
+router.get('/', function(req, res) {
+  User.find()
+  .then((users) => {
+    res.send(users)
+  })
 });
+
+// NEW
+
+// SHOW ONE
+
+// CREATE
+
+// EDIT
+
+// UPDATE
+
+// DELETE
 
 module.exports = router;
