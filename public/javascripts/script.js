43c840ebedb6c95fa852ebe30c58edf24428cefd
elfein <elfein99@gmail.com>
2018-09-27 15:32:51 -0400

    refactored code

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,17 +1,17 @@
 let modal = $('#delete-modal')
-    let modalOverlay = $('#delete-modal-overlay')
-    let modalOpenLink = $('#delete')
+let modalOverlay = $('#delete-modal-overlay')
+let modalOpenLink = $('#delete')
 
-    modalOverlay.on('click', closeModal)
+modalOverlay.on('click', closeModal)
 
-    function closeModal() {
-        modal.toggleClass('closed')
-        modalOverlay.toggleClass('closed')
-    }
+function closeModal() {
+    modal.toggleClass('closed')
+    modalOverlay.toggleClass('closed')
+}
 
-    modalOpenLink.on('click', openModal)
+modalOpenLink.on('click', openModal)
 
-    function openModal() {
-        modal.toggleClass('closed')
-        modalOverlay.toggleClass('closed')
-    }
+function openModal() {
+    modal.toggleClass('closed')
+    modalOverlay.toggleClass('closed')
+}
